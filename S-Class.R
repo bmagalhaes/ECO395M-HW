aa635cf9d6fa941b5db8f53592303617af97bc9f
bmagalhaes <60456840+bmagalhaes@users.noreply.github.com>
2020-02-06 09:38:28 -0600

    Update S-Class.R

--- S-Class.R
+++ S-Class.R
@@ -1,4 +1,5 @@
 library(mosaic)
+library(FNN)
 
 sclass = read.csv('sclass.csv', head=1)
 summary(sclass)
@@ -63,8 +64,9 @@ for(i in 3:nrow(X_train)) {
   knn_resultpred = cbind(knn_resultpred,ypred_knn)
 }
 
-names(knn_resultpred) = c("N",3:332)
-knn_resultpred = subset(knn_resultpred[2:332])
+vector_1 = c("N",3:332)
+vector_1 = sub("^", "K", vector_1)
+colnames(knn_resultpred) <- vector_1
 
 knn_resultrsme = data.frame(K=c(),rsme=c())
 i=3
@@ -76,7 +78,6 @@ for(i in 3:nrow(X_train)) {
 }
 
 names(knn_resultrsme) = c("K", "rsme")
-
 k_min = with(knn_resultrsme, K[rsme == min(rsme)])
 
 ggplot(data = knn_resultrsme) + 
@@ -85,23 +86,110 @@ theme_bw(base_size=18) +
 geom_vline(xintercept = k_min, 
                color = "purple", size=1)
 
-pred_knn = knn_resultpred$"19"
-pred_knn = as.data.frame(pred_knn)
-D_test = arrange(D_test, mileage)
-pred_knn = mutate(pred_knn, mileage = D_test$mileage)
-pred_knn = mutate(pred_knn, price = D_test$price)
+knn_min = knn.reg(train = X_train, test = X_test, y = y_train, k=k_min)
+ypred_knn = as.data.frame(knn_min$pred)
+names(ypred_knn) = c("ypred_knn")
 
-ggplot(data = pred_knn) + 
+D_test = arrange(D_test, mileage)
+ypred_knn = mutate(ypred_knn, mileage = D_test$mileage)
+ypred_knn = mutate(ypred_knn, price = D_test$price)
+ypred_knn = mutate(ypred_knn, ypred_knn3 = knn_resultpred$K3)
+ypred_knn = mutate(ypred_knn, ypred_knn75 = knn_resultpred$K75)
+ypred_knn = mutate(ypred_knn, ypred_lm = ypred_lm2)
+
+ggplot(data = ypred_knn) + 
   geom_point(mapping = aes(x = mileage, y = price), color='lightgrey') + 
   theme_bw(base_size=18) +
-  geom_path(aes(x = mileage, y = pred_knn), color='blue')
+  geom_path(aes(x = mileage, y = ypred_knn), color='red', size=1) +
+  geom_path(aes(x = mileage, y = ypred_knn3), color='blue') +
+  geom_path(aes(x = mileage, y = ypred_knn75), color='purple') +
+  geom_path(aes(x = mileage, y = ypred_lm), color='black')
+
+##### NOW FOR THE OTHER CAR MODEL ####
+
+M = nrow(sclass65AMG)
+M_train = floor(0.8*M)
+M_test = M - M_train
+
+# select random sample to include in the training set
+train_ind2 = sample.int(M, M_train, replace=FALSE)
+
+# Formalise training and testing sets
+E_train = sclass65AMG[train_ind2,]
+E_test = sclass65AMG[-train_ind2,]
+
+#housekeeping, just arrange as per mileage
+E_test = arrange(E_test, mileage)
+head(E_test)
+
+# Seperate training and testing sets into features (mileage: X) and outcome (price: y)
+X2_train = select(E_train, mileage)
+y2_train = select(E_train, price)
+X2_test = select(E_test, mileage)
+y2_test = select(E_test, price)
+
+lm2 = lm(price ~ poly(mileage, 2), data=E_train)
+ypred_lm22 = predict(lm2, E_test)
+
+ggplot(data = E_test) + 
+  geom_point(mapping = aes(x = mileage, y = price), color='lightgrey') + 
+  theme_bw(base_size=18) + geom_path(aes(x = mileage, y = ypred_lm22), color='blue')
+
+knn_resultpred2 <- c(1:59)
+knn_resultpred2 = as.data.frame(knn_resultpred2)
+
+i=3
+for(i in 3:nrow(X2_train)) {
+  knn_mod2 = knn.reg(train = X2_train, test = X2_test, y = y2_train, k=i)
+  ypred_knn2 = knn_mod2$pred
+  knn_resultpred2 = cbind(knn_resultpred2,ypred_knn2)
+}
+
+vector_2 = c("N",3:233)
+vector_2 = sub("^", "K", vector_2)
+colnames(knn_resultpred2) = vector_2
+
+knn_resultrsme2 = data.frame(K=c(),rsme=c())
+i=3
+for(i in 3:nrow(X2_train)) {
+  knn_mod2 = knn.reg(train = X2_train, test = X2_test, y = y2_train, k=i)
+  ypred_knn2 = knn_mod2$pred
+  rsme_knn2 = rsme(y2_test, ypred_knn2) 
+  knn_resultrsme2 =  rbind(knn_resultrsme2, c(i,rsme_knn2))
+}
+
+names(knn_resultrsme2) = c("K", "rsme")
+k_min2 = with(knn_resultrsme2, K[rsme == min(rsme)])
+
+ggplot(data = knn_resultrsme2) + 
+  geom_point(mapping = aes(x = K, y = rsme), color='lightgrey') + 
+  theme_bw(base_size=18) +
+  geom_vline(xintercept = k_min2, 
+             color = "purple", size=1)
 
 
+knn_min2 = knn.reg(train = X2_train, test = X2_test, y = y2_train, k=k_min2)
+ypred_knn2 = as.data.frame(knn_min2$pred)
+names(ypred_knn2) = c("ypred_knn2")
+
+E_test = arrange(E_test, mileage)
+ypred_knn2 = mutate(ypred_knn2, mileage = E_test$mileage)
+ypred_knn2 = mutate(ypred_knn2, price = E_test$price)
+ypred_knn2 = mutate(ypred_knn2, ypred_knn3 = knn_resultpred2$K3)
+ypred_knn2 = mutate(ypred_knn2, ypred_knn75 = knn_resultpred2$K75)
+ypred_knn2 = mutate(ypred_knn2, ypred_lm = ypred_lm22)
+
+ggplot(data = ypred_knn2) + 
+  geom_point(mapping = aes(x = mileage, y = price), color='lightgrey') + 
+  theme_bw(base_size=18) +
+  geom_path(aes(x = mileage, y = ypred_knn2), color='red', size=1) +
+  geom_path(aes(x = mileage, y = ypred_knn3), color='blue') +
+  geom_path(aes(x = mileage, y = ypred_knn75), color='purple') +
+  geom_path(aes(x = mileage, y = ypred_lm), color='black')
 
 
 
 ##### FROM HERE DOWN I DIDN'T CHECK ####
-
 
 
 
